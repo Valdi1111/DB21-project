2069678f0e06dfac302dbc235914fe7eeeeafa2d
Davide Valdifiori <davide.valdifiori.dv@gmail.com>
2022-05-21 20:54:47 +0200

    Add bootstrap Edit Footer.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,12 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Shop from './components/Shop';
 
-/*<header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header>*/
-
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <BrowserRouter>
           <Header />
           <Routes>
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,46 +1,34 @@
-import './footer.css';
+import instagram from '../icons/instagram-brands.svg'
+import twitter from '../icons/twitter-brands.svg'
+import facebook from '../icons/facebook-brands.svg'
+import copyright from '../icons/copyright-regular.svg'
 
 function Footer() {
     return (
-        <footer className="container">
-            <div>
-                <div><a href="#">anymeme</a></div>
-                <section>
-                    <p>Creator: Aldini, Alam, Valdifiori</p>
-                    <p>contact@anymeme.com</p>
+        <footer>
+            <div className="row">
+                <div className="my-3 col-md-12 text-md-start col-lg-8 order-lg-0"><a href="#">anymeme</a></div>
+                <section className="mb-3 col-md-6 text-md-start col-lg-4 order-lg-2">
+                    <p className="m-0">Creator: Aldini, Alam, Valdifiori</p>
+                    <p className="m-0">contact@anymeme.com</p>
                 </section>
-                <section>
-                    <p>Via Cristoforo Colombo, 35</p>
-                    <p>+39 328 70 43436</p>
+                <section className="mb-3 col-md-6 text-md-end col-lg-4 order-lg-3 text-lg-center">
+                    <p className="m-0">Via Cristoforo Colombo, 35</p>
+                    <p className="m-0">+39 328 70 43436</p>
                 </section>
-                <section>
-                    <p>We are hiring, write to our manager</p>
-                    <p>management@anymeme.com</p>
+                <section className="mb-3 col-md-6 text-md-start col-lg-4 order-lg-4 text-lg-end">
+                    <p className="m-0">We are hiring, write to our manager</p>
+                    <p className="m-0">management@anymeme.com</p>
                 </section>
-                <section>
-                    <a href="#">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram"
-                             className="svg-inline--fa fa-instagram fa-w-14" role="img"
-                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><title>Instagram</title>
-                            <path
-                                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-                        </svg>
+                <section className="mb-3 col-md-6 text-md-end col-lg-4 order-lg-1">
+                    <a className="ms-2" href="#">
+                        <img src={instagram} alt="Instagram" style={{width: 20}}/>
                     </a>
-                    <a href="#">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitter"
-                             className="svg-inline--fa fa-twitter fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                             viewBox="0 0 512 512"><title>Twitter</title>
-                            <path
-                                d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-                        </svg>
+                    <a className="ms-2" href="#">
+                        <img src={twitter} alt="Twitter" style={{width: 20}}/>
                     </a>
-                    <a href="#">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-square"
-                             className="svg-inline--fa fa-facebook-square fa-w-14" role="img"
-                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><title>Facebook</title>
-                            <path
-                                d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path>
-                        </svg>
+                    <a className="ms-2" href="#">
+                        <img src={facebook} alt="Facebook" style={{width: 20}}/>
                     </a>
                     <div>
                         <input type="checkbox" id="theme-switch" onChange="onThemeChange(this)"/>
@@ -49,14 +37,9 @@ function Footer() {
                     </div>
                 </section>
             </div>
-            <section>
-                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="copyright"
-                     className="svg-inline--fa fa-copyright fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 512 512"><title>Copyright</title>
-                    <path
-                        d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 448c-110.532 0-200-89.451-200-200 0-110.531 89.451-200 200-200 110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200zm107.351-101.064c-9.614 9.712-45.53 41.396-104.065 41.396-82.43 0-140.484-61.425-140.484-141.567 0-79.152 60.275-139.401 139.762-139.401 55.531 0 88.738 26.62 97.593 34.779a11.965 11.965 0 0 1 1.936 15.322l-18.155 28.113c-3.841 5.95-11.966 7.282-17.499 2.921-8.595-6.776-31.814-22.538-61.708-22.538-48.303 0-77.916 35.33-77.916 80.082 0 41.589 26.888 83.692 78.277 83.692 32.657 0 56.843-19.039 65.726-27.225 5.27-4.857 13.596-4.039 17.82 1.738l19.865 27.17a11.947 11.947 0 0 1-1.152 15.518z"></path>
-                </svg>
-                <p>2022 anymeme.com, s.o.s.</p>
+            <section className="d-flex justify-content-center">
+                <img src={copyright} alt="Facebook" style={{width: 10}}/>
+                <p className="mb-0 ms-2">2022 anymeme.com, s.o.s.</p>
             </section>
         </footer>
     );
