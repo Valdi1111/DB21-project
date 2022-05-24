417b0b0e6afd91051dd6fa9b5d5651bcb5cf6a15
Davide Valdifiori <davide.valdifiori.dv@gmail.com>
2022-05-24 23:56:06 +0200

    Add filter for products api call. Add images to products/:id api call.

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,12 +6,25 @@ const response = require("../methods/response");
 router.get(
     "/",
     (req, res, next) => {
+        let query = `SELECT p.id, p.title, p.description, i.path AS cover, p.price, p.discount 
+                        FROM product p LEFT JOIN product_has_image i ON i.product_id = p.id AND i.is_cover = 1 
+                        WHERE p.visible = 1`;
+        if (req.body.max_price) {
+            query += ` AND p.price <= ${db.escape(req.body.max_price)}`;
+        }
+        if (req.body.min_price) {
+            query += ` AND p.price >= ${db.escape(req.body.min_price)}`;
+        }
+        if (req.body.search) {
+            query += ` AND p.title LIKE ${db.escape("%" + req.body.search + "%")}`;
+        }
+        query += `;`;
         db.query(
-            `SELECT id, title, description, image, price FROM product;`,
+            query,
             (err, results, fields) => {
                 // db error
                 if (err) {
-                    return response.internalError();
+                    return response.internalError(res, err);
                 }
                 return res.json(results);
             }
@@ -22,14 +35,35 @@ router.get(
 router.get(
     "/:id",
     (req, res, next) => {
+        let r = {};
+        // search product
         db.query(
             `SELECT * FROM product WHERE id = ${db.escape(req.params.id)};`,
             (err, results, fields) => {
                 // db error
                 if (err) {
-                    return response.internalError();
+                    return response.internalError(res, err);
                 }
-                return res.json(results[0]);
+                // nothing found
+                if (!results) {
+                    return res.json(r);
+                }
+                // add product to response
+                r.product = results[0];
+                // search images
+                db.query(
+                    `SELECT path, is_cover FROM product_has_image WHERE product_id = ${db.escape(req.params.id)};`,
+                    (err, results, fields) => {
+                        // db error
+                        if (err) {
+                            return response.internalError(res, err);
+                        }
+                        // add images to response
+                        r.images = results;
+                        // send response
+                        return res.json(r);
+                    }
+                );
             }
         );
     }
