d1e37cc41cc8881a9015872e334f5c9f702cb5c8
Davide Valdifiori <davide.valdifiori.dv@gmail.com>
2022-07-16 11:55:44 +0200

    Add coupon redeem.

--- backend/routes/buyer/profile.js
+++ backend/routes/buyer/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const profile = require("../../services/profile");
+const coupon = require("../../services/coupon");
+const response = require("../../methods/response");
 
 router.get(
     "/",
@@ -21,6 +23,31 @@ router.put(
         try {
             const {name, username, fiscal_code, gender} = req.body;
             await profile.editBuyerProfile(req.user_id, name, username, fiscal_code, gender);
+            return res.send();
+        } catch (err) {
+            console.error("Error on PUT buyer/profile.");
+            next(err);
+        }
+    }
+);
+
+router.post(
+    "/redeem",
+    async (req, res, next) => {
+        try {
+            const {code} = req.body;
+            const results = await coupon.getByCode(code);
+            if(!results.length) {
+                return response.notFound(res, "coupon_not_found", "No coupon found with code " + code + "!");
+            }
+            if(results.expired) {
+                return response.notFound(res, "coupon_expired", "Coupon with code " + code + " is expired!");
+            }
+            if(results.used) {
+                return response.notFound(res, "coupon_already_redeemed", "Coupon with code " + code + " has already been redeemed!");
+            }
+            await coupon.use(req.user_id, results[0].id);
+            await profile.increaseBuyerBalance(req.user_id, results[0].value);
             return res.send();
         } catch (err) {
             console.error("Error on PUT buyer/profile.");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import AuthService from "./services/AuthService";
-import {useEffect, useState} from "react";
-import axios from "axios";
 import {api_user_url} from "./services/ApiUrls";
+import {useEffect, useState} from "react";
+import {toast} from "wc-toast";
+import axios from "axios";
 import RequireAuth from "./services/RequireAuth";
 import ErrorPage from "./components/ErrorPage";
 import Home from "./components/Home";
@@ -12,46 +12,51 @@ import Header from "./components/header/Header";
 import Footer from "./components/Footer";
 import Product from "./components/product/Product";
 import Settings from "./components/settings/Settings";
-import Order from "./components/order/Order";
+import Order from "./components/Order";
 import Orders from "./components/Orders";
+import RequireBuyer from "./services/RequireBuyer";
+import Cart from "./components/Cart";
+import RequireSeller from "./services/RequireSeller";
+import MyProduct from "./components/myproducts/product/MyProduct";
+import MyProducts from "./components/myproducts/MyProducts";
+import Chat from "./components/Chat";
 
 function App() {
-    AuthService.updateToken();
-    //const [token, setToken] = useState("");
-    const [auth, setAuth] = useState(false);
-    useEffect(() => {
-        if (AuthService.isLoggedIn()) {
-            fetchData();
-        } else {
-            setAuth(false);
-        }
-    }, []);
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [auth, setAuth] = useState(null);
 
-    function fetchData() {
+    useEffect(() => {
+        if(!token) {
+            return;
+        }
         axios
             .get(
                 `${api_user_url}data`,
-                {headers: AuthService.authHeader()}
+                {headers: {"x-access-token": token}}
             )
             .then(
                 res => {
-                    AuthService.user = res.data;
-                    setAuth(true);
+                    const a = {};
+                    a["token"] = token;
+                    a["user"] = res.data;
+                    setAuth(a)
                 },
                 err => {
-                    logout();
+                    toast.error("An error occurred...");
+                    setToken("");
                 }
-            )
-    }
+            );
+    }, [token]);
 
-    function login(token) {
-        AuthService.setToken(token);
-        fetchData();
+    function login(t) {
+        localStorage.setItem("token", t);
+        setToken(t);
     }
 
     function logout() {
-        AuthService.logout();
-        setAuth(false);
+        localStorage.removeItem("token");
+        setToken("");
+        setAuth(null);
     }
 
     return (
@@ -60,17 +65,21 @@ function App() {
                 <Routes>
                     <Route path="login" element={<></>}/>
                     <Route path="register" element={<></>}/>
-                    <Route path="*" element={<Header logged={auth} logout={logout}/>}/>
+                    <Route path="*" element={<Header auth={auth} logout={logout}/>}/>
                 </Routes>
                 <Routes>
                     <Route index element={<Home/>}/>
                     <Route path="login" element={<Login login={login}/>}/>
                     <Route path="register" element={<Login login={login}/>}/>
                     <Route path="shop" element={<Shop/>}/>
-                    <Route path="product/:id" element={<Product/>}/>
-                    <Route path="orders" element={<Orders/>}/>
-                    <Route path="order/:id" element={<Order/>}/>
-                    <Route path="settings/*" element={<RequireAuth><Settings/></RequireAuth>}/>
+                    <Route path="chat" element={<Chat/>}/>
+                    <Route path="product/:id" element={<Product auth={auth}/>}/>
+                    <Route path="cart" element={<RequireBuyer auth={auth}><Cart auth={auth}/></RequireBuyer>}/>
+                    <Route path="orders" element={<RequireBuyer auth={auth}><Orders auth={auth}/></RequireBuyer>}/>
+                    <Route path="order/:id" element={<RequireBuyer auth={auth}><Order auth={auth}/></RequireBuyer>}/>
+                    <Route path="my-products" element={<RequireSeller auth={auth}><MyProducts auth={auth}/></RequireSeller>}/>
+                    <Route path="my-product/:id" element={<RequireSeller auth={auth}><MyProduct auth={auth}/></RequireSeller>}/>
+                    <Route path="settings/*" element={<RequireAuth auth={auth}><Settings auth={auth}/></RequireAuth>}/>
                     <Route path="*" element={<ErrorPage title="Page not found" name="Home" link="/"/>}/>
                 </Routes>
                 <Routes>
